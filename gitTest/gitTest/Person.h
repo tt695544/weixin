d9d3817281341b500fea969f62ada79b2212c8e2
mumu <695122695@qq.com>
2015-09-17 18:31:40 -0700

    添加了一个类

---
+++ gitTest/gitTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  gitTest
+//
+//  Created by Mac on 15/9/17.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ gitTest/gitTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  gitTest
+//
+//  Created by Mac on 15/9/17.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
